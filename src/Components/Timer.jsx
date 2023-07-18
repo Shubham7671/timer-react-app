9353de3cd65b18cf01a194dff4170b3ccfddc6fb
Shubham7671 <sk767196@gmail.com>
2023-07-18 19:03:02 +0530

    done

--- src/Components/Timer.jsx
+++ src/Components/Timer.jsx
@@ -1,66 +1,91 @@
 import React, { useEffect } from 'react'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+import alarmSound from '../assests/alarmSound.mp3'
+
+
 
 export default function Timer() {
   let [min, setMin] = useState(0);
   let [sec, setSec] = useState(0);
   let [time, setTime] = useState(0);
-  let [intervalId, setIntervalId] = useState(null);
+  let [sound, setSound] = useState(false);
+  let intervalId = useRef(null);
+
 
 
   useEffect(() => {
-
     const clearTimer = () => {
-      clearInterval(intervalId);
+      clearInterval(intervalId.current);
+      console.log("last")
     }
-
     return clearTimer;
   }, [])
 
 
 
   const startTimer = () => {
-
-    if (intervalId != null) {
+    setSound(false)
+    if (intervalId.current != null) {
       return;
     }
     if (time == 0) {
       setTime((min * 60) + sec)
     }
 
-    setIntervalId(setInterval(() => {
-      setTime(prev => prev - 1)
-    }, 1000))
+    intervalId.current = setInterval(() => {
+      setTime((prev) => {
+        if (prev == 1) {
+          clearInterval(intervalId.current);
+          intervalId.current = null
+          setSound(true);
+
+        }
+        return prev - 1;
+      })
+    }, 1000)
   }
+
   const stopTimer = () => {
-    clearInterval(intervalId)
-    setIntervalId(null);
+    clearInterval(intervalId.current)
+    intervalId.current = null
   }
+
   const resetTimer = () => {
     stopTimer();
     setTime(0);
-    setMin(0);
-    setSec(0);
-
-
+    // setMin(0);
+    // setSec(0);
+  }
+  let play=()=>{
+    new Audio(alarmSound).play();
+    setSound(false)
+  }
+  function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    const formattedTime = `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
+    return formattedTime;
   }
 
   return (
-    <div style={{ textAlign: "center", width: "50%", margin: "200px auto", border: "2px solid black", padding: "50px" }}>
-      <h1 >Timer : {time}</h1>
+    <div style={{ textAlign: "center", width: "50%", margin: "200px auto", border: "1px solid grey", padding: "50px" }}>
+      <h1 >Timer : {formatTime(time)}</h1>
       <div>
         <div
           style={time > 0 ? { display: "none" } : { display: "block" }}>
           <input type="number" onChange={(e) => { setMin(Number(e.target.value)) }} />Minutes
-          <input type="number" onChange={(e) => { setSec(Number(e.target.value)) }} />Seconds
+          <input type="number"  onChange={(e) => { setSec(Number(e.target.value)) }} />Seconds
         </div>
 
 
 
-        <button onClick={startTimer} >Start</button>
-        <button onClick={stopTimer} >Stop</button>
-        <button onClick={resetTimer} >Reset</button>
+        <button  style={ {background:"green",color:"white"}} onClick={startTimer} disabled={min == 0 && sec == 0 ? true : false}>{time>0?"Resume":"Start"}</button>
+        <button  style={time <= 0 ? { display: "none"} : { display: "inline" ,background:"red",color:"white"}} onClick={stopTimer} >Pause</button>
+        <button  style={time <= 0 ? { display: "none"} : { display: "inline" ,background:"blue",color:"white"}}onClick={resetTimer} >Reset</button>
       </div>
+  
+      {sound ? play():null}
+    
     </div>
   )
 }
